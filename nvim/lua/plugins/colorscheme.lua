4e6b4c2a9e68a94c63aba275bcd9dcf78c07b3ef
OrlandoTellez <espadachinkinno@gmail.com>
2026-01-05 18:50:57 -0600

    feat: agregar tema transparente en colescheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,60 @@
+-- TEMA GRUVBOX
+--return {
+--  {
+--    "iibe/gruvbox-high-contrast",
+--    name = "gruvbox-high-contrast",
+--    lazy = false,
+--    priority = 1000,
+--    config = function()
+--      -- Opciones para gruvbox-high-contrast
+--      vim.g.gruvbox_bold = 0
+--      vim.g.gruvbox_italic = 0
+--      vim.g.gruvbox_transparent_bg = 0
+--      vim.g.gruvbox_contrast_dark = "hard"
+--      vim.g.gruvbox_sign_column = "bg1"
+--      vim.g.gruvbox_number_column = "bg0"
+--      vim.g.gruvbox_color_column = "bg1"
+--
+--      -- Establecer fondo oscuro
+--      vim.o.background = "dark"
+--
+--      -- Aplicar el tema
+--      vim.cmd([[colorscheme gruvbox-high-contrast]])
+--
+--      -- Ajustes para neo-tree
+--      vim.cmd([[hi NeoTreeFloatNormal guifg=#ebdbb2 guibg=#3c3836]])
+--      vim.cmd([[hi NeoTreeFloatBorder guifg=#665c54 guibg=#3c3836]])
+--    end,
+--  },
+--}
+
+-- TEMA TRANSPARENTE
 return {
   {
-    "iibe/gruvbox-high-contrast",
-    name = "gruvbox-high-contrast",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
+    lazy = false,
     config = function()
-      -- Opciones para gruvbox-high-contrast
-      vim.g.gruvbox_bold = 0
-      vim.g.gruvbox_italic = 0
-      vim.g.gruvbox_transparent_bg = 0
-      vim.g.gruvbox_contrast_dark = "hard"
-      vim.g.gruvbox_sign_column = "bg1"
-      vim.g.gruvbox_number_column = "bg0"
-      vim.g.gruvbox_color_column = "bg1"
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true, -- 👈 CLAVE
+        term_colors = true,
+        integrations = {
+          treesitter = true,
+          native_lsp = {
+            enabled = true,
+          },
+          neo_tree = true,
+          lualine = true,
+          cmp = true,
+          telescope = true,
+          gitsigns = true,
+          noice = true,
+        },
+      })
 
-      -- Establecer fondo oscuro
-      vim.o.background = "dark"
-
-      -- Aplicar el tema
-      vim.cmd([[colorscheme gruvbox-high-contrast]])
-
-      -- Ajustes para neo-tree
-      vim.cmd([[hi NeoTreeFloatNormal guifg=#ebdbb2 guibg=#3c3836]])
-      vim.cmd([[hi NeoTreeFloatBorder guifg=#665c54 guibg=#3c3836]])
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
